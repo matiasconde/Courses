7cd3cb3d606b825113dcbb11959595a515b499ef
Matias <matias.leandro.conde@gmail.com>
2018-10-30 15:29:37 -0300

    Remove duplicated directory

--- Manual de Python/num mayormenor - Manual Python.py
+++
@@ -1,9 +0,0 @@
-a=int(input("ingrese primer numero entero: "))
-if a>0:
-    print("el número es mayor que 0")
-if a==0:
-    print("el número es 0")
-if a<0:
-    print("el número es menor que 0")
-
-input("finish")
--- Old excercises/Ejercicio probabilidad condicional test +.py
+++
@@ -1,9 +0,0 @@
-def P_EA_(p):
-    return (0.005*p)/(p*0.005+(1-p)*0.995)
-
-print(P_EA_(0.9997488068324542))
-
-def desired_value(x):
-    return 0.995/(1-0.005*x)
-
-print(desired_value(0.95))
