8a3e98e1c22bcf0fa5b4d77c91f52bf3c0190b63
matiasconde <matias.leandro.conde@gmail.com>
2019-04-23 09:49:31 -0300

    R Step 2

---
+++ 40_Data Visualization in R/Scatter Plots for Exploratory Analysis-276.r
@@ -0,0 +1,66 @@
+## 1. Importing and Modifying Data ##
+
+reviews_2 <- read_csv("movie_reviews_2.csv")
+
+reviews_2 <- reviews_2 %>% 
+  select("FILM","RT_user_norm","Metacritic_user_nom","IMDB_norm","Fandango_Ratingvalue")
+
+categories <- c("FILM","Rotten_Tomatoes","Metacritic","IMDB","Fandango")
+
+colnames(reviews_2) <- categories
+
+## 2. Understanding Relationships Between Variables ##
+
+ggplot(data = reviews_2) + 
+  aes(x = Fandango, y = Rotten_Tomatoes) + 
+  geom_point()
+
+
+## 3. Creating Informative Scatter Plots ##
+
+ggplot(data = reviews_2) +
+   aes(x = Fandango, y = Rotten_Tomatoes) +
+   geom_point(alpha = 0.3) + 
+   xlim(1,5) +
+   ylim(1,5) +
+   theme(panel.background = element_rect(fill = "white"))
+
+
+
+
+## 4. Creating Multiple Scatter Plots ##
+
+ggplot(data = reviews_2) +
+   aes(x = Fandango, y = IMDB) +
+   geom_point(alpha = 0.3) +
+   xlim(1,5) +
+   ylim(1,5) +
+   theme(panel.background = element_rect(fill = "white"))
+  
+ggplot(data = reviews_2) +
+   aes(x = Fandango, y = Metacritic) +
+   geom_point(alpha = 0.3) +
+   xlim(1,5) +
+   ylim(1,5) +
+   theme(panel.background = element_rect(fill = "white"))
+  
+
+## 5. Write a Function to Create Multiple Scatter Plots ##
+
+
+
+create_scatter <- function(x,y) {
+  ggplot(data = reviews_2) + 
+     aes_string(x = x, y = y) +
+     geom_point(alpha = 0.3) +
+     xlim(1,5) +
+     ylim(1,5) +
+     theme(panel.background = element_rect(fill = "white"))
+}
+
+x_var = names(reviews_2)[5]
+y_var = names(reviews_2)[2:4]
+
+map2(x_var, y_var, create_scatter)
+
+x_var
